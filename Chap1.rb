cf72671aee7041dd1dd305a23bd10c2936ccfe03
Ben <blenox@shortridgeacademy.com>
2017-09-22 15:11:13 -0400

    Chap1 and Chap2

---
+++ Chap1.rb
@@ -0,0 +1,5 @@
+puts 'Ruby is awesome!!!!!1111!!!!1!!!1!'
+puts 2 + 5
+puts 24 * 10
+puts 'Hi ' + 'there!'
+puts 2 + 2
---
+++ Chap2.rb
@@ -0,0 +1,30 @@
+puts 'Property of His Royal Highness, the King'.length
+puts 'Property of His Royal Highness, the King'.reverse
+kings_string = 'A string fit for a king'
+wiener_dog_weight = 22
+puts wiener_dog_weight
+smalls_weight = 22
+pounds_lost = 4
+smalls_new_weight = smalls_weight - pounds_lost
+puts '18'.reverse
+puts 100 + 17
+puts 50 - 20
+puts 10 * 10
+puts 40 / 20
+puts 22.next
+puts 22.pred
+double_quotes = "A string's the thing"
+single_quotes = 'for a springly King'
+kings_string = "Property of His Royal Highness, the King"
+string_reflection = kings_string.reverse
+times_to_echo = 3
+string_echo = kings_string * times_to_echo
+puts kings_string
+puts string_reflection
+puts string_echo
+puts 'Hello!' * 3
+puts gadzooks.length
+puts 4.next
+bacon_consistency = 'chunky'
+number_of_bacon_strips = 3
+puts bacon_consistency
